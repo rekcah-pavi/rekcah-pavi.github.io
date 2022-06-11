07bc1b6109322e3c5716f5d4cf9c62cf52131f73
rekcah-pavi <55050467+rekcah-pavi@users.noreply.github.com>
2022-06-11 11:02:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,5 +5,16 @@
 </head>
 <body>
 <h1><?php echo "Hello" ?></h1>
+
+<?php
+$host = 'google.com';
+if($socket =@ fsockopen($host, 80, $errno, $errstr, 30)) {
+echo 'online!';
+fclose($socket);
+} else {
+echo 'offline.';
+}
+?>
+
 </body>
 </html>
